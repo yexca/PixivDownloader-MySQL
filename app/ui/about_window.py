cedae95bb89fce4674578cb218f574c50b040136
yexca <yexcano@gmail.com>
2025-01-03 20:01:44 +0900

    关于页面

--- app/ui/about_window.py
+++ app/ui/about_window.py
@@ -7,6 +7,15 @@ class AboutWindow(QWidget):
 
     def init_ui(self):
         layout = QVBoxLayout()
-        label = QLabel("About this application.")
-        layout.addWidget(label)
+        self.description = QLabel()
+        self.description.setText(
+            """<p>作者: <a href="https://github.com/yexca">yexca</a> <br/>
+            没有任何错误处理，非常简单的程序<br/>
+            具体请访问 <a href="https://blog.yexca.net/archives/211" rel="noopener">https://blog.yexca.net/archives/211</a>
+             查看吧</p>"""
+        )
+        self.description.setOpenExternalLinks(True)  # Enable hyperlink functionality
+        self.description.setWordWrap(True)  # Enable word wrapping
+        self.description.setMaximumHeight(100)
+        layout.addWidget(self.description)
         self.setLayout(layout)
